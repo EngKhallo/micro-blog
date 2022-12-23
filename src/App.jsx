efa483757f7b8e248a6a5a4aee15b40220d74128
EngKhallo <engkhalid.ict@gmail.com>
2022-12-23 20:28:45 +0300

    Main Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
 }
 
 export default App;
