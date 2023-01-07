3db20f88e0c8d5ce939f8fb1e2ce5b1a0eaf17b6
EngKhallo <engkhalid.ict@gmail.com>
2023-01-07 14:28:24 +0300

    Blogs Form : change State

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,21 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
-        <div className="create">
-            <h2>Add a New Blog</h2>
-            <form>
-                <label>Blog Title: </label>
-                <input type="text" required />
-                <label>Blog Body: </label>
-                <textarea required></textarea>
-                <label>Blog Author: </label>
-                <select>
-                    <option value="khalid">Khalid</option>
-                    <option value="ahmed">Ahmed</option>
-                </select>
-                <button>Add Blog</button>
-            </form>
-        </div>
-     );
-}
- 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <p>{title}</p>
+      <p>{body}</p>
+      <p>{author}</p>
+      <form>
+        <label>Blog Title: </label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body: </label>
+        <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <label>Blog Author: </label>
+        <select
+        value = {author}
+        onChange={(e) => setAuthor(e.target.value)}
+        >
+        <option value="">Select Author:</option>
+          <option value="khalid">Khalid</option>
+          <option value="ahmed">Ahmed</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
+};
+
 export default Create;
